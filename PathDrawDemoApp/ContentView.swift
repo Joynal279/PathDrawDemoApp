bd0140d64d90c093891121e8ee7d6f4993b6bc8a
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-27 21:04:29 +0600

    custom shape added

--- PathDrawDemoApp/ContentView.swift
+++ PathDrawDemoApp/ContentView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Path { path in
+                path.move(to: CGPoint(x: 50, y: 10))
+                path.addLine(to: CGPoint(x: 10, y: 350))
+                path.addLine(to: CGPoint(x: 300, y: 300))
+                path.closeSubpath()
+            }
+            .fill(Color.green)
+            
+            MyShape()
+                .fill(Color.red)
+                .frame(width: 360, height: 350)
         }
-        .padding()
+    }
+}
+
+//Custom Shape
+struct MyShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.maxY), control: CGPoint(x: rect.midX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.closeSubpath()
+        return path
     }
 }
 
