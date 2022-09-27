a76225d0b66ec234a9e6a0a97cd574852391e20b
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-27 20:44:54 +0600

    Initial Commit

---
+++ PathDrawDemoApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PathDrawDemoApp
+//
+//  Created by JOYNAL ABEDIN on 27/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PathDrawDemoApp/PathDrawDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathDrawDemoAppApp.swift
+//  PathDrawDemoApp
+//
+//  Created by JOYNAL ABEDIN on 27/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PathDrawDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
